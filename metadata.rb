b0970733c01c6b36191003ee2095f04a3d8783ce
Kevin Gilpin <kgilpin@gmail.com>
2013-12-08 13:15:35 -0500

    i have actually been able to login

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              'pam-ldap'
+maintainer        'Conjur, Inc.'
+maintainer_email  'kgilpin@conjur.net'
+license           'Apache 2.0'
+description       'Installs Conjur PAM+LDAP'
+version           '0.0.1'
+
+supports :ubuntu
